5e58a030f4aebe440bc24990f0e48942f6148094
galleu <59324021+galleu@users.noreply.github.com>
2021-09-28 13:49:01 +0300

    Added main.py

---
+++ main.py
@@ -0,0 +1,44 @@
+import requests
+import mimetypes
+import hashlib
+
+class Tebi:
+    def __init__(self, bucket, **kwargs):
+        self.bucket = "https://" + bucket
+        self.auth = kwargs.get('auth', None)
+
+
+    def GetObject(self, key):
+        headers={}
+        if (self.auth):
+            headers["Authorization"] = "TB-PLAIN: " + self.auth
+        response = requests.get(self.bucket+"/"+key, headers=headers)
+        return response
+        
+    def PutObject(self, key, obj, **kwargs):
+        file = kwargs.get('file', None)
+        mime = kwargs.get('ContentType', None)
+        auth = kwargs.get('auth', "TB-PLAIN: "+self.auth)
+        
+        CacheControl = kwargs.get('CacheControl', None)
+
+        data = obj        
+        if (mime != None and mime == "auto" and file != None):
+            mime =  mimetypes.guess_type(file)[0]
+        
+        headers = {}
+        if (mime != None):
+            headers["Content-Type"] = mime
+
+        if (CacheControl != None):
+            headers["Cache-Control"] = CacheControl
+
+        if (self.auth):
+            headers["Authorization"] = auth
+
+        if (file and not data):
+            data = open(file, "rb")
+            
+        headers["Content-MD5"] = hashlib.md5(data).hexdigest()
+
+        response = requests.put(self.bucket + +"/"+key, headers=headers)
